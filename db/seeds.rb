f789d3d3542aafbacc063c21e9baaca030e958fe
alexandru <boncut.alex10@gmail.com>
2021-06-08 11:08:03 -0500

    added_seeds_file

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create(username: "Alex", email: "admin@gmail.com", password: "1234")
+Location.create(name: "Cedar Point", description: "An amazing fun park", address: "1 Oak Park Rd, Ohio")
+Review.create(title: "Great experience", content: "This has been an amazing experience.")
+Attraction.create(name: "Rollercoster", description: "2 min of high speed fun riding", rating: "5")

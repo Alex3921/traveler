33042cac837f14e3590d57628ba071c7cc20249e
alexandru <boncut.alex10@gmail.com>
2021-06-08 22:05:52 -0500

    add_validations_secure_pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 require_relative './concerns/slugifiable.rb'
 
 class User < ActiveRecord::Base
+  has_secure_password
+  
   has_many :reviews
   has_many :attractions, through: :reviews
+
+  validates :username, :email, :password_digest, presence: true
+  validates :username, :email, uniqueness: { case_sensitive: false }
 end

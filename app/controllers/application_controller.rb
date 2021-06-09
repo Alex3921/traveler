7bf74255ce36bc39f5d75fc3fc2e23a803959307
alexandru <boncut.alex10@gmail.com>
2021-06-08 20:08:26 -0500

    added_helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,32 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    use Rack::Flash
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, 'abcdefghijklmnopqrstuvwxyz'
   end
 
+
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
   end
 
 end

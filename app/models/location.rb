fa62f664d9b7eca73ef01600b38e038fc3088401
alexandru <boncut.alex10@gmail.com>
2021-06-13 20:57:59 -0500

    Relationship update.

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,7 @@ class Location < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
   has_many :attractions
+  belongs_to :user
 
   validates :name, uniqueness: { case_sensitive: false }
 end

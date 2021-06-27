ade7cf9814071b4006e6d25f6cddb76c3a174a9b
alexandru <boncut.alex10@gmail.com>
2021-06-26 19:36:03 -0500

    Edited the search query

--- app/models/concerns/api.rb
+++ app/models/concerns/api.rb
@@ -1,7 +1,7 @@
 class API
 
   def self.get_data(query, page = "1", orientation = "landscape")
-      response = RestClient.get("https://api.unsplash.com/search/photos?page=#{page}&orientation=#{orientation}&query=#{query}-view&client_id=#{ENV['UNSPLASH_CLIENT_ID']}")
+      response = RestClient.get("https://api.unsplash.com/search/photos?page=#{page}&orientation=#{orientation}&query=#{query}&client_id=#{ENV['UNSPLASH_CLIENT_ID']}")
       JSON.parse(response) 
   end
 
